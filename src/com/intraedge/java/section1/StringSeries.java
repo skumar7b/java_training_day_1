161b5c7fc531b7b7490f3720e0b4338323a30f16
vasanthj1978 <44370553+vasanthj1978@users.noreply.github.com>
2019-04-20 09:16:02 -0700

    Create StringSeries.java

---
+++ src/com/intraedge/java/section1/StringSeries.java
@@ -0,0 +1,13 @@
+/**
+ * Print a series of Strings to the console
+ */
+
+public class StringSeries {
+	
+	public static void main(String[] args) {
+		for(String str: args) {
+			System.out.println(str);
+		}
+	}
+
+}
