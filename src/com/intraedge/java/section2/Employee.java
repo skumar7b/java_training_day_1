7da3510c554a2fa181223b4023ceb45dbc6f0116
vasanthj1978 <44370553+vasanthj1978@users.noreply.github.com>
2019-04-20 15:14:35 -0700

    Update Employee.java

--- src/com/intraedge/java/section2/Employee.java
+++ src/com/intraedge/java/section2/Employee.java
@@ -4,10 +4,9 @@
  * 2. create a new test class with a main() method. You can call the test class as EmployeeTest.java.
  * 3. inside this test class, instantiate an object from this class and call its methods
  * (hint - use the 'new' operator to create an object).
- * 4. ask your instructor if you need help :)
  */
 public class Employee {
-	private String name;
-	private String department;
-	private int age;
+	String name;
+	String department;
+	int age;
 }
