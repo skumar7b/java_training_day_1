57d05821b236060fb2d79442ffbcf78907486565
vasanthj1978 <44370553+vasanthj1978@users.noreply.github.com>
2019-05-13 18:09:39 -0700

    Create MyFirstProgram.java

---
+++ src/com/intraedge/java/section1/MyFirstProgram.java
@@ -0,0 +1,17 @@
+/**
+ * 1. save this file as MyFirstProgram.java in a folder of your choice
+ * 2. navigate to the folder and compile this class using the command "javac MyFirstProgram.java"
+ * 3. list the contents of the folder again. If your program compiled correctly, you should see
+ * a file called MyFirstProgram.class. This is the compiled bytecode of your program
+ * 4. run the bytecode using the command "java MyFirstProgram". This command will load this
+ * bytecode in the JVM and execute it.
+ * 5. you should see the line "My first Java program..." printed omn your console
+ */
+
+public class MyFirstProgram {
+	
+	public static void main(String[] args) {
+		System.out.println("My first Java program...");
+	}
+
+}
