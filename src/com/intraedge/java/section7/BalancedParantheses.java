35a71c59723e175250bb95b52bfb9edd7c264908
skumar7b <55125632+skumar7b@users.noreply.github.com>
2019-09-12 03:45:39 -0400

    Update BalancedParantheses.java

--- src/com/intraedge/java/section7/BalancedParantheses.java
+++ src/com/intraedge/java/section7/BalancedParantheses.java
@@ -5,5 +5,54 @@
  * Hint: Use a Stack and HashMap
  */
 public class BalancedParantheses {
-	
+	package com;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+
+
+public class BalancedParanthesis {
+	public static void main(String args[]){
+        String brackets = "{}{{{}}}";
+        
+        Stack<Character> stack = new Stack<>();
+        boolean isBalanced = true;
+ 
+        for (int i = 0; i < brackets.length(); i++) {
+            char bracket = brackets.charAt(i);
+            if (bracket == '{' || bracket == '[' || bracket == '(') {
+                stack.push(bracket);
+            } else {
+                if (bracket == '}') {
+                    bracket = '{';
+                } else if (bracket == ')') {
+                    bracket = '(';
+                } else if (bracket == ']') {
+                    bracket = '[';
+                }
+ 
+                if (stack.empty()) {
+                    isBalanced = false;
+                    break;
+                }
+ 
+                if (bracket == stack.peek()) {
+                    stack.pop();
+                } else {
+                    isBalanced = false;
+                    break;
+                }
+            }
+        }
+ 
+        if (isBalanced) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
+	}
+
+}
+
 }
